b18257c2276725800e156f6e27c82fd59f740bd7
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-10-26 11:30:33 +0700

    Initial commit

---
+++ lib/App/SerializeUtils.pm
@@ -0,0 +1,39 @@
+package App::SerializeUtils;
+
+use 5.010001;
+use strict;
+use warnings;
+
+# VERSION
+
+1;
+#ABSTRACT: Utilities for serialization tasks
+
+=head1 SYNOPSIS
+
+ $ script-that-produces-json | json2yaml
+
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities related to
+serialization:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Data::Dump>
+
+L<JSON>
+
+L<PHP::Serialization>
+
+L<Sereal>
+
+L<Storable>
+
+L<YAML>
+
+=cut
